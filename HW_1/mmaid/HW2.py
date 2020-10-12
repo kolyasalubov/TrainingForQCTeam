859157d12264e0d33e5f71378ef4aba454278b61
MMaidaniuk <gilza1991@gmail.com>
2020-10-12 19:02:33 +0300

    HW1 part 2

---
+++ HW_1/mmaid/HW2.py
@@ -0,0 +1,10 @@
+a = float(input("Input a = "))
+b = float(input("Input b = "))
+print("a+b=", a+b)
+print("a-b=", a-b)
+print("a*b=", a*b)
+if b != 0:
+    print("a/b=", a/b)
+else:
+    print("Infinite")
+print("a**b=", a**b)
