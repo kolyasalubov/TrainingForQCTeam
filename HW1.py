08db67090195ebb9b7bb4e40928847e1f2b733e4
Artem Burumenskyi <burumenskyart@gmail.com>
2020-10-12 13:26:09 +0300

    HomeWork1

---
+++ HW1.py
@@ -0,0 +1,11 @@
+# # #module 1 
+# name= input ("what is your name?\n", )
+# print ("Ok,", name.capitalize() , "how old are you?" )
+# age = input ()
+# print ("hmmm,", age, "it's perfect age",'where do you live')
+# city = input ()
+# print ("Summary:\n" , 'name =', name, "\n", 'age =', age , "\n",'city=', city.capitalize())
+
+print ('insert some numeric variables and get result of math function')
+from deffs import math
+math() 
