5915cf7c52d5dc195471944fd80e60e2ca5b01cf
Liubov Koliasa <lkoliasa@mail.com>
2020-10-19 18:04:19 +0300

    add homework1

---
+++ HW_1/Koliasa/hm1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print('a + b =', (a + b))
+print('a - b =', (a - b))
+print('a * b =', (a * b))
+if b == 0:
+    print("You can't divide on Zero")
+else:
+    print('a / b =', (a / b))
