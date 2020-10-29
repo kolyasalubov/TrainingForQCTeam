873ce63a49b9e9f5c73c0a33538e9a251608398a
Anastasiia Maiier <nastyona_best@rambler.ru>
2020-10-29 20:40:06 +0200

    Add 4 and 5 hw

---
+++ HW_1/Amaiier_hw1_task2.py
@@ -0,0 +1,6 @@
+name = input ("What is your name?\n")
+print ("Hello,", name)
+age = input ("How old are you?\n")
+print ("My age is", age)
+city = input ("Where do you live?\n")
+print ("I live in", city)
