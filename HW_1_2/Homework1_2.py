eef05b80480d36dd192342dd2b9b09c4d5c99356
Dmytro <dante13172406@gmail.com>
2020-11-01 13:01:47 +0200

    Homework1_2

---
+++ HW_1_2/Homework1_2.py
@@ -0,0 +1,12 @@
+print("Enter a")
+a = input()
+a = float(a)
+print("Enter b")
+b = input()
+b = float(b)
+r_1 = a + b
+r_2 = a - b
+r_3 = a * b
+r_4 = a / b
+r_5 = a ** b
+print(f"a + b = {r_1} \na - b = {r_2}\na * b = {r_3}\na / b = {r_4}\na ** b = {r_5}")
