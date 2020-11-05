10d654858dc97b3c4e92c0f9bb9ab2b1c5126a0e
MMaidaniuk <gilza1991@gmail.com>
2020-11-05 17:46:37 +0200

    part two hw6

--- HW_6/mmaid/2.py
+++ HW_6/mmaid/2.py
@@ -3,7 +3,7 @@ import re
 while True:
     pswd = input("Enter your password ")
     if (re.search("[a-z]", pswd) and re.search("[A-Z]", pswd) and 
-    re.search("\d", pswd) and re.search("[!@#$%^&*_+-=]", pswd) and
+    re.search(r"\d", pswd) and re.search("[!@#$%^&*_+-=]", pswd) and
     len(pswd) >= 6 and len(pswd) <= 16):
         print("Your password is strong")
         break
