cf5d8a2c5968b28025660cfd9753069226ecff57
MMaidaniuk <gilza1991@gmail.com>
2020-11-18 16:18:52 +0200

    part 2 HW 9

---
+++ HW_9/mmaid/PositiveNumber.py
@@ -0,0 +1,17 @@
+class MyError(Exception): 
+    def __init__(self, data): 
+        self.data = data
+    def __str__(self):
+        return repr(self.data)
+
+def check_positive(number):
+    try:
+        if number >= 1:
+            return f"You input positive number: {number}"
+        else:
+            raise MyError(f"You input negative number: {number}. Try again.")
+    except MyError as my:
+        return my.data
+    except:
+        return "Error type: ValueError!"
+
