db13555ec36dd8284a5b27b5b727cae1ea6fe445
Dmytro <dante13172406@gmail.com>
2020-11-22 14:57:56 +0200

    Homework5_1

---
+++ HW_5/Homework5_1.py
@@ -0,0 +1,14 @@
+print('Enter number a: ')
+a = int (input())
+print('Enter number b: ')
+b = int (input())
+
+
+def biggest(c, d):
+    ''' This method defines the biggest number '''
+    return max(c, d)
+
+
+print(biggest(a, b))
+
+
